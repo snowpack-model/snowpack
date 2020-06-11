98642d921f9f024e73f8e7b7ad203c331f2315cc
Nander Wever <nwever@gmail.com>
2020-06-11 11:16:26 -0600

    Switching from data_converter to meteo_timeseries (the latter is compiled by default).

--- Scripts/create_smet_from_netcdf/create_forcing.sh
+++ Scripts/create_smet_from_netcdf/create_forcing.sh
@@ -3,7 +3,8 @@
 # Retrieve script variables
 model=$1
 yr=$2
-output_res=$3 # In seconds
+output_res_in_seconds=$3						# In seconds
+output_res_in_minutes=$(echo ${output_res_in_seconds} / 60 | bc)	# In minutes
 
 # Print Statements
 echo "Working on model: ${model}"
@@ -24,11 +25,16 @@ mkdir -p output/${model}_${yr}
 cd io_files
 
 # Create new ini file for each year
-sed 's/VSTATIONS_REFRESH_RATE=3600/VSTATIONS_REFRESH_RATE='${output_res}'/' ${model}.ini > ${model}_${yr}.ini 
-sed -i 's/VSTATIONS_REFRESH_OFFSET=3600/VSTATIONS_REFRESH_OFFSET='${output_res}'/' ${model}_${yr}.ini
-sed -i 's/PSUM::arg1::cst=3600/PSUM::arg1::cst='${output_res}'/' ${model}_${yr}.ini
+sed 's/VSTATIONS_REFRESH_RATE=3600/VSTATIONS_REFRESH_RATE='${output_res_in_seconds}'/' ${model}.ini > ${model}_${yr}.ini
+sed -i 's/VSTATIONS_REFRESH_OFFSET=3600/VSTATIONS_REFRESH_OFFSET='${output_res_in_seconds}'/' ${model}_${yr}.ini
+sed -i 's/PSUM::arg1::cst=3600/PSUM::arg1::cst='${output_res_in_seconds}'/' ${model}_${yr}.ini
 sed -i 's/METEOPATH=..\/output\//METEOPATH=..\/output\/'${model}'_'${yr}'\//' ${model}_${yr}.ini
 
 # Create .smet files for each year
-#../data_converter ${yr}-01-01T00:30:00 ${yr}-12-31T23:30:00 60 ${model}_${yr}.ini > ../log/${model}_${yr}.log 2>&1
-../data_converter ${yr}-01-01T00:30:00 ${yr}-01-01T06:30:00 60 ${model}_${yr}.ini > ../log/${model}_${yr}.log 2>&1
+if [ ${model} == "MERRA-2" ]; then
+	# MERRA-2 has output each :30
+	../meteoio_timeseries -b ${yr}-01-01T00:30:00 -e ${yr}-12-31T23:30:00 -c ${model}_${yr}.ini -s ${output_res_in_minutes} > ../log/${model}_${yr}.log 2>&1
+else
+	# Other models have output each :00, and the following line assumes the last time step is 21:00 (i.e., 3-hourly output)
+	../meteoio_timeseries -b ${yr}-01-01T00:00:00 -e ${yr}-12-31T21:00:00 -c ${model}_${yr}.ini -s ${output_res_in_minutes} > ../log/${model}_${yr}.log 2>&1
+fi
