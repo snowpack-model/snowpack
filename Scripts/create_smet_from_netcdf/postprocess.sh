20b2c4bd8c15350a3794af035a4d96fb9ab99f44
EricKeenan <eric.keenan@colorado.edu>
2020-06-10 21:58:49 -0600

    Update postprocess.sh such that it can handle any model

--- Scripts/create_smet_from_netcdf/postprocess.sh
+++ Scripts/create_smet_from_netcdf/postprocess.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Retrieve script variables
+model=$1
+
 # Concactenate yearly files into a single timeseries
-g=$(ls -d output/output_[0-9][0-9][0-9][0-9] | tail -1)
+g=$(ls -d output/${model}_[0-9][0-9][0-9][0-9] | tail -1)
 for f in ${g}/*smet
 do
         g=$(basename ${f} .smet)
-        cat output/output_*/${g}.smet | awk '{if(/^SMET/) {header=1}; if((data==1 && header==0) || (data==0 && header==1)) {print}; if(/\[DATA\]/) {data=1; header=0}}' > output/${g}.smet
+        cat output/${model}_*/${g}.smet | awk '{if(/^SMET/) {header=1}; if((data==1 && header==0) || (data==0 && header==1)) {print}; if(/\[DATA\]/) {data=1; header=0}}' > output/${g}.smet
 done
 
 # Zip output .smet files
