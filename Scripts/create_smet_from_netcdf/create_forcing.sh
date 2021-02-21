f6b50bad3aa6a79a845349ffa819bde3e3a12247
Nander Wever <nawe3645@shas0136.rc.int.colorado.edu>
2021-02-21 10:53:51 -0700

    Fixing the create_forcing.sh script.

--- Scripts/create_smet_from_netcdf/create_forcing.sh
+++ Scripts/create_smet_from_netcdf/create_forcing.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 shopt -s expand_aliases	# Make sure aliases work in non-interactive shells
+# Check if mawk exist, otherwise create alias
+if ! command -v mawk &> /dev/null
+then
+	alias mawk='awk'
+fi
 
 # Retrieve script variables
 model=$1
@@ -32,12 +37,13 @@ mkdir -p output/${model}_${yr}
 # Retrieve directory with netCDF and create file links, if netCDF files are split by year
 flag_netcdf_files_are_linked=0
 grid2dpath=$(fgrep GRID2DPATH ./io_files/MERRA-2.ini | mawk -F= '{gsub(/^[ \t]+/,"",$NF); gsub(/[ \t]+$/,"",$NF); print $NF}')	# Use gsub to remove trailing and leading white spaces
-if [ -e ${grid2dpath}/*${yr}*nc ]; then
+list_of_nc_files=$(find ${grid2dpath} -name "*${yr}*")
+if [ ! -z "${list_of_nc_files}" ]; then
 	# Create dir to link the NetCDF files
 	flag_netcdf_files_are_linked=1
 	rm -r input/${model}_${yr}
 	mkdir -p input/${model}_${yr}
-	for ncf in ${grid2dpath}/*${yr}*nc; do
+	for ncf in ${list_of_nc_files}; do
 		ln -s ${ncf} input/${model}_${yr}/
 	done
 fi
@@ -51,8 +57,8 @@ sed 's/..\/output\//..\/output\/'${model}'_'${yr}'\//' ${model}.ini > ${inifile}
 
 # Set paths correctly in case we linked the netcdf files
 if (( ${flag_netcdf_files_are_linked} )); then
-	sed -i 's/.*METEOPATH.*/GRID2DPATH = ../input/${model}_${yr}' ${inifile}
-	sed -i 's/.*GRID2DPATH.*/GRID2DPATH = ../input/${model}_${yr}' ${inifile}
+	sed -i 's/.*METEOPATH.*/METEOPATH = ..\/input\/'${model}'_'${yr}'\//' ${inifile}
+	sed -i 's/.*GRID2DPATH.*/GRID2DPATH = ..\/input\/'${model}'_'${yr}'\//' ${inifile}
 fi
 
 # Retrieve temporal resolution from ini file:
