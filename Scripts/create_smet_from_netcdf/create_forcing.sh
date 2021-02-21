b8591990604389caf1a4ac3997d74afc704868fe
Nander Wever <nwever@gmail.com>
2021-02-21 10:11:01 -0700

    Create links for netcdf files, only when they are split by year. By linking only the files containing data for a specific year in a separate directory for each year, this should speed up the processing.

--- Scripts/create_smet_from_netcdf/create_forcing.sh
+++ Scripts/create_smet_from_netcdf/create_forcing.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+shopt -s expand_aliases	# Make sure aliases work in non-interactive shells
 
 # Retrieve script variables
 model=$1
@@ -24,9 +25,22 @@ export LD_LIBRARY_PATH=$(pwd)/usr/lib/:${LD_LIBRARY_PATH}
 module purge
 ml intel; ml proj; ml netcdf
 
-# Create output directory 
+# Create output directory
 rm -r output/${model}_${yr}
 mkdir -p output/${model}_${yr}
+
+# Retrieve directory with netCDF and create file links, if netCDF files are split by year
+flag_netcdf_files_are_linked=0
+grid2dpath=$(fgrep GRID2DPATH ./io_files/MERRA-2.ini | mawk -F= '{gsub(/^[ \t]+/,"",$NF); gsub(/[ \t]+$/,"",$NF); print $NF}')	# Use gsub to remove trailing and leading white spaces
+if [ -e ${grid2dpath}/*${yr}*nc ]; then
+	# Create dir to link the NetCDF files
+	flag_netcdf_files_are_linked=1
+	rm -r input/${model}_${yr}
+	mkdir -p input/${model}_${yr}
+	for ncf in ${grid2dpath}/*${yr}*nc; do
+		ln -s ${ncf} input/${model}_${yr}/
+	done
+fi
 
 # Move into io_files dir, where computation will be peformed
 cd io_files
@@ -34,6 +48,12 @@ cd io_files
 # Create new ini file for each year
 inifile=${model}_${yr}.ini
 sed 's/..\/output\//..\/output\/'${model}'_'${yr}'\//' ${model}.ini > ${inifile}
+
+# Set paths correctly in case we linked the netcdf files
+if (( ${flag_netcdf_files_are_linked} )); then
+	sed -i 's/.*METEOPATH.*/GRID2DPATH = ../input/${model}_${yr}' ${inifile}
+	sed -i 's/.*GRID2DPATH.*/GRID2DPATH = ../input/${model}_${yr}' ${inifile}
+fi
 
 # Retrieve temporal resolution from ini file:
 output_res_in_seconds=$(fgrep VSTATIONS_REFRESH_RATE ${inifile} | awk -F\= '{i=$NF; gsub(/[ \t]/, "", i); print i}')	# In seconds
